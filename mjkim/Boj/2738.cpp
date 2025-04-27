fab40956aa3c124a0a8cf2b6f8ff4b3755e24267
MJ37031 <rlaalswl0520@naver.com>
2025-04-27 11:04:19 +0900

    Solve 2738.cpp

--- mjkim/Boj/2738.cpp
+++ mjkim/Boj/2738.cpp
@@ -12,14 +12,14 @@ int main()
 
 	scanf("%d %d", &n, &m);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)  // 배열 a 입력 
 	{
 		for (j = 0; j < m; j++)
 		{
 			scanf("%d", &a[i][j]);
 		}
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)  // 배열 b입력
 	{
 		for (j = 0; j < m; j++)
 		{
@@ -27,7 +27,7 @@ int main()
 		}
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)  // 배열 합 구하기
 	{
 		for (j = 0; j < m; j++)
 		{
